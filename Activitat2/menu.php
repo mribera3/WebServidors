456fd69284bfc72afecaedab0b71ea39aefc79ec
mribera3 <mribera@infobosccoma.net>
2016-11-14 19:41:02 +0100

    Update menu.php

--- Activitat2/menu.php
+++ Activitat2/menu.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
+<meta charset=UTF-8">
 <title>Menú</title>
 </head>
 <body>        
 <h1>Menú</h1>
 <?php
 session_start();
-if (isset($_SESSION['admin'])) {
-echo "&nbsp;&nbsp;Has fet login amb l'usuari:<br></br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$_SESSION['admin'];  
+if (isset($_SESSION['usuari'])) {
+echo "&nbsp;&nbsp;Has fet login amb l'usuari:<br></br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".$_SESSION['usuari'];  
 }
 ?>
 <p>Aquí tens un llistat de les següents opcions a triar<p>
 <ul>
 	<li><a href="comprovar_divisor.php">Comprovar divisors</a></li>
-    <li><a href="Conjetura_collatz.php">Conjentura collatz</a></li>
+    <li><a href="conjentura_collatz.php">Conjentura collatz</a></li>
     <li><a href="sortir.php">Sortir</a></li>
 </ul>           
 </body>
